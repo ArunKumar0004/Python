7220f63a29dbac5bde273514cd48f3ee2e0ca391
ArunKumar0004 <31059805+ArunKumar0004@users.noreply.github.com>
2017-10-20 15:50:49 +0530

    Add files via upload

---
+++ mathop.py
@@ -0,0 +1,23 @@
+#program by declaring 10 variables and use all the mathematical Operations
+#Mathematical opertations
+a=1
+b=2
+c=0
+d=4
+e=5
+f=0
+g=3
+h=4 
+i=0
+k=0
+j=0
+c=a+b
+f=e-d
+i=g*h
+k=h/d
+j=d%g
+print "Sum of 1 and 2 is:",c
+print "Difference of 5 and 4 is:",f
+print "Product of 3 and 4 is:",i
+print "Division of 4 and 4 is:",k
+print "Modulo of 4 and 3 is:",j
