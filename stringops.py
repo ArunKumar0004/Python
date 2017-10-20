6b163e4a535e1d29b281a512d9a2063af841f95e
ArunKumar0004 <31059805+ArunKumar0004@users.noreply.github.com>
2017-10-20 16:13:36 +0530

    Add files via upload

---
+++ stringops.py
@@ -0,0 +1,7 @@
+#String operations
+str="programmer"
+print "r occurs",str.count("r"),"times"
+print "g is at:",str.index("g")
+print "After replacing m with Y:",str.replace("m","Y")
+str1=str.replace("m","Y")
+print "Final String:",str1
